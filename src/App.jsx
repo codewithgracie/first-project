49c526d14c6f21d02f140d181202121a83771cfc
codewithgracie <emmanuelgrace411@gmail.com>
2025-06-23 15:33:49 +0100

    project progress commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar.jsx';
+import Hero from './components/Hero.jsx';
+import Analytics from './components/Analytics.jsx';
+
+
+const App = () => {
+  return (
+    <div>
+      < Navbar />
+      < Hero />
+      <Analytics />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Analytics.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Laptop from '../assets/Laptop.png'
+
+const Analytics = () => {
+  return (
+    <div className='w-full bg-white py-16 px-4'>
+        <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+          <img src={Laptop} alt="A laptop showing digital data" />
+          <div>
+            <p>DATA ANALYTICS DASHBOARD</p>
+            <h1>Manage Data Analytics Centrally</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas nihil veniam asperiores maiores ea sit vero quo. Sapiente distinctio aliquam porro, id repellendus, obcaecati libero culpa nemo, dolore ex nisi.</p>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Analytics
