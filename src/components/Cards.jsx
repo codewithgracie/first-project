4066f3929c1354cfee1eb1e83059ef3beb0f5492
codewithgracie <emmanuelgrace411@gmail.com>
2025-07-30 01:11:51 +0100

    commit  card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Single from '../assets/Single.png';
-import Double from '../assets/double.png';
-import Triple from '../assets/triple.png';
+import Double from '../assets/Double.png';
+import Triple from '../assets/Triple.png';
 
 const Cards = () => {
     return (
@@ -16,7 +16,29 @@ const Cards = () => {
                         <p className='py-2 border-b mx-8' >1 granted User </p>
                         <p className='py-2 border-b mx-8' >Send up to 2 GB</p>
                     </div>
-                    <button>Start Trail</button>
+                    <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 '>Start Trail</button>
+                </div>
+                <div className="w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300">
+                    <img className='w-20 mx-auto mt-[-3rem] bg-transparent'  src={Double} />
+                    <h2 className='text2xl font-bold text-center py-8 ' >Single user</h2>
+                    <p className='text-center text-4xl font-bold' >$149</p>
+                    <div className='text-center font-medium' >
+                        <p className='py-2 border-b mx-8 mt-8' >500 GB Storage</p>
+                        <p className='py-2 border-b mx-8' >1 granted User </p>
+                        <p className='py-2 border-b mx-8' >Send up to 2 GB</p>
+                    </div>
+                    <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 '>Start Trail</button>
+                </div>
+                <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+                    <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Triple} />
+                    <h2 className='text2xl font-bold text-center py-8 ' >Single user</h2>
+                    <p className='text-center text-4xl font-bold' >$149</p>
+                    <div className='text-center font-medium' >
+                        <p className='py-2 border-b mx-8 mt-8' >500 GB Storage</p>
+                        <p className='py-2 border-b mx-8' >1 granted User </p>
+                        <p className='py-2 border-b mx-8' >Send up to 2 GB</p>
+                    </div>
+                    <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 '>Start Trail</button>
                 </div>
             </div>
         </div>
